93b85cf6907fc6adcc8c7bc0b6a2bea00508b388
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-07 01:05:35 -0400

    Consolidate CMake arguments

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,12 @@ cd build
 
 set BUILD_CONFIG=Release
 
-@rem Reduce build times and package size by removing unused stuff
-set CMAKE_CUSTOM=-DLLVM_INCLUDE_TESTS=OFF ^
-    -DLLVM_INCLUDE_DOCS=OFF
-
 cmake -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" ^
     -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    %CMAKE_CUSTOM% ^
+    -DLLVM_INCLUDE_TESTS=OFF ^
+    -DLLVM_INCLUDE_DOCS=OFF ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
