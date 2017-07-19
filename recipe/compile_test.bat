e4505af32526576207fe01fb14f06726dd957fac
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-07-19 00:25:08 -0400

    Revert "Dropped `-nostdlib` and system libraries"
    This reverts commit f28c7311c697c743023cdda2b26bc220f846c07e.

--- recipe/compile_test.bat
+++ recipe/compile_test.bat
@@ -15,7 +15,7 @@ if errorlevel 1 exit 1
 pushd test_sources
 if errorlevel 1 exit 1
 for /f "usebackq" %%i in (`dir /b ^| findstr /i "^.*\.cpp$"`) do (
-    clang++ -nostdinc++ -isystem "%LIBRARY_INC%\c++\v1" -I "%LIBRARY_INC%" -L "%LIBRARY_LIB%" -lc++ -O2 -g %%i -o test_clang.exe
+    clang++ -nostdinc++ -nostdlib -isystem "%LIBRARY_INC%\c++\v1" -I "%LIBRARY_INC%" -L "%LIBRARY_LIB%" -lc++ -lvcruntime -lucrt -lmsvcrt -O2 -g %%i -o test_clang.exe
     if errorlevel 1 exit 1
     test_clang
     if errorlevel 1 exit 1
@@ -26,7 +26,7 @@ if errorlevel 1 exit 1
 pushd test_sources\cpp11
 if errorlevel 1 exit 1
 for /f "usebackq" %%i in (`dir /b ^| findstr /i "^.*\.cpp$"`) do (
-    clang++ -nostdinc++ -isystem "%LIBRARY_INC%\c++\v1" -I "%LIBRARY_INC%" -L "%LIBRARY_LIB%" -lc++ -std=c++11 -O2 -g %%i -o test_clang.exe
+    clang++ -nostdinc++ -nostdlib -isystem "%LIBRARY_INC%\c++\v1" -I "%LIBRARY_INC%" -L "%LIBRARY_LIB%" -lc++ -lvcruntime -lucrt -lmsvcrt -std=c++11 -O2 -g %%i -o test_clang.exe
     if errorlevel 1 exit 1
     test_clang
     if errorlevel 1 exit 1
