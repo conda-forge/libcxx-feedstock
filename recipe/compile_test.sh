8b7119ba879fd8bd272e37b63be19dec75132893
Matthew R Becker <becker.mr@gmail.com>
2019-10-17 09:37:54 -0500

    remove test due to conda build bug

--- recipe/compile_test.sh
+++ recipe/compile_test.sh
@@ -7,27 +7,27 @@ LINK_FLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
 FILES=test_sources/*.c
 for f in $FILES
 do
-    clang -O2 -g $f $LINK_FLAGS
+    $PREFIX/bin/clang -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/*.cpp
 for f in $FILES
 do
-    clang++ -stdlib=libc++ -O2 -g $f $LINK_FLAGS
+    $PREFIX/bin/clang++ -stdlib=libc++ -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/cpp11/*.cpp
 for f in $FILES
 do
-    clang++ -stdlib=libc++ -std=c++11 -O2 -g $f $LINK_FLAGS
+    $PREFIX/bin/clang++ -stdlib=libc++ -std=c++11 -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/cpp14/*.cpp
 for f in $FILES
 do
-    clang++ -stdlib=libc++ -std=c++14 -O2 -g $f $LINK_FLAGS
+    $PREFIX/bin/clang++ -stdlib=libc++ -std=c++14 -O2 -g $f $LINK_FLAGS
     ./a.out
 done
