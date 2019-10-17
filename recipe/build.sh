ccefce1c16d963a9d220582eb19de6986eeb2832
Matthew R Becker <becker.mr@gmail.com>
2019-10-16 22:10:41 -0500

    try this

--- recipe/build.sh
+++ recipe/build.sh
@@ -24,22 +24,31 @@ make -j${CPU_COUNT}
 make install
 cd ..
 
-# Build libcxxabi
-cd libcxxabi
-mkdir build && cd build
+if [[ "$target_platform" != "osx-64" ]]; then
+    # Build libcxxabi
+    cd libcxxabi
+    mkdir build && cd build
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLIBCXXABI_LIBCXX_PATH=$SRC_DIR \
-  -DLIBCXXABI_LIBCXX_INCLUDES=$SRC_DIR/include \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DLLVM_INCLUDE_DOCS=OFF \
-  ..
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXXABI_LIBCXX_PATH=$SRC_DIR \
+      -DLIBCXXABI_LIBCXX_INCLUDES=$SRC_DIR/include \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
 
-make -j${CPU_COUNT}
-make install
-cd ../..
+    make -j${CPU_COUNT}
+    make install
+    cd ../..
+
+    cxxabi_inc=$SRC_DIR/libcxxabi/include
+    cxxabi_lib=$PREFIX/lib
+else
+    # on osx we point libc++ to the system libc++abi
+    cxxabi_inc=${CONDA_BUILD_SYSROOT}/usr/include
+    cxxabi_lib=${CONDA_BUILD_SYSROOT}/usr/lib
+fi
 
 # Build libcxx with libcxxabi
 mkdir build2
@@ -49,16 +58,11 @@ cmake \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DLIBCXX_CXX_ABI=libcxxabi \
-  -DLIBCXX_CXX_ABI_INCLUDE_PATHS=$SRC_DIR/libcxxabi/include \
-  -DLIBCXX_CXX_ABI_LIBRARY_PATH=$PREFIX/lib \
+  -DLIBCXX_CXX_ABI_INCLUDE_PATHS=${cxxabi_inc} \
+  -DLIBCXX_CXX_ABI_LIBRARY_PATH=${cxxabi_lib} \
   -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_INCLUDE_DOCS=OFF \
   ..
 
 make -j${CPU_COUNT}
 make install
-
-if [[ "$target_platform" == "osx-64" ]]; then
-    # on osx we point libc++ to the system libc++abi
-    install_name_tool -change "@rpath/libc++abi.dylib" "/usr/lib/libc++abi.dylib" $PREFIX/lib/libc++.dylib
-fi
