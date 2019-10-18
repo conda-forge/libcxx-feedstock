f506be9340714a02636ea434c0a67a2202e6a90c
Matthew R Becker <becker.mr@gmail.com>
2019-10-18 18:59:39 -0500

    bundle not on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,3 @@
-mkdir build
-cd build
-
 LLVM_PREFIX=$PREFIX
 
 if [[ "$target_platform" == "osx-64" ]]; then
@@ -14,10 +11,48 @@ export LDFLAGS="$LDFLAGS -L$LLVM_PREFIX/lib -Wl,-rpath,$LLVM_PREFIX/lib -L$BUILD
 export PATH="$LLVM_PREFIX/bin:$PATH"
 
 if [[ "$target_platform" != "osx-64" ]]; then
+    # build libcxx first
+    mkdir build
+    cd build
+
     cmake \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
-      -DLIBCXX_CXX_ABI=none \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
+
+    make -j${CPU_COUNT}
+    make install
+    cd ..
+
+    # now build libcxxabi
+    cd libcxxabi
+    mkdir build && cd build
+
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXXABI_LIBCXX_PATH=$SRC_DIR \
+      -DLIBCXXABI_LIBCXX_INCLUDES=$SRC_DIR/include \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
+
+    make -j${CPU_COUNT}
+    make install
+    cd ../..
+
+    # now rebuild libcxx with libcxxabi
+    mkdir build2
+    cd build2
+
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXX_CXX_ABI=libcxxabi \
+      -DLIBCXX_CXX_ABI_INCLUDE_PATHS=$SRC_DIR/libcxxabi/include \
+      -DLIBCXX_CXX_ABI_LIBRARY_PATH=$PREFIX/lib \
       -DLLVM_INCLUDE_TESTS=OFF \
       -DLLVM_INCLUDE_DOCS=OFF \
       ..
@@ -25,6 +60,7 @@ if [[ "$target_platform" != "osx-64" ]]; then
     make -j${CPU_COUNT}
     make install
 
+    cd ..
 else
     # on osx we point libc++ to the system libc++abi
     cmake \
@@ -42,6 +78,6 @@ else
 
     # on osx we point libc++ to the system libc++abi
     install_name_tool -change "@rpath/libc++abi.1.dylib" "/usr/lib/libc++abi.dylib" $PREFIX/lib/libc++.1.0.dylib
-fi
 
-cd ..
+    cd ..
+fi
