12e10c254e9d9e36c8bf52e3f56fcbd037b64eef
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-10-19 08:27:14 -0500

    ENH fix rtti issues on osx (#45)
    * fix rtti issues on osx
    
    * MNT: Re-rendered with conda-build 3.18.10, conda-smithy 3.6.0, and conda-forge-pinning 2019.10.11
    
    * try this
    
    * display the symbols
    
    * actually run the tests
    
    * try this
    
    * try this
    
    * try this again
    
    * roll back conda build
    
    * roll back conda build
    
    * remove test due to conda build bug
    
    * MNT: Re-rendered with conda-build 3.18.10, conda-smithy 3.6.0, and conda-forge-pinning 2019.10.11
    
    * formatting
    
    * try this
    
    * this is still needed
    
    * this is still needed
    
    * maybe this
    
    * weird conflict
    
    * try this
    
    * maybe this?
    
    * maybe this?
    
    * wtf
    
    * try this
    
    * this one
    
    * try this
    
    * no abi
    
    * try again
    
    * try this
    
    * try this again
    
    * add back windows stuff
    
    * maybe this
    
    * much simpler build
    
    * put back ignore run exports
    
    * bundle not on osx
    
    * bugz and tweaks
    
    * add linking flag for linux
    
    * Update compile_test.sh
    
    * Use $INSTALL_NAME_TOOL

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,3 @@
-mkdir build
-cd build
-
 LLVM_PREFIX=$PREFIX
 
 if [[ "$target_platform" == "osx-64" ]]; then
@@ -13,47 +10,77 @@ export CFLAGS="$CFLAGS -I$LLVM_PREFIX/include -I$BUILD_PREFIX/include"
 export LDFLAGS="$LDFLAGS -L$LLVM_PREFIX/lib -Wl,-rpath,$LLVM_PREFIX/lib -L$BUILD_PREFIX/lib -Wl,-rpath,$BUILD_PREFIX/lib"
 export PATH="$LLVM_PREFIX/bin:$PATH"
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DLLVM_INCLUDE_DOCS=OFF \
-  ..
+if [[ "$target_platform" != "osx-64" ]]; then
+    # build libcxx first
+    mkdir build
+    cd build
 
-make -j${CPU_COUNT}
-make install
-cd ..
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
 
-# Build libcxxabi
-cd libcxxabi
-mkdir build && cd build
+    make -j${CPU_COUNT}
+    make install
+    cd ..
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLIBCXXABI_LIBCXX_PATH=$SRC_DIR \
-  -DLIBCXXABI_LIBCXX_INCLUDES=$SRC_DIR/include \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DLLVM_INCLUDE_DOCS=OFF \
-  ..
+    # now build libcxxabi
+    cd libcxxabi
+    mkdir build && cd build
 
-make -j${CPU_COUNT}
-make install
-cd ../..
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXXABI_LIBCXX_PATH=$SRC_DIR \
+      -DLIBCXXABI_LIBCXX_INCLUDES=$SRC_DIR/include \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
 
-# Build libcxx with libcxxabi
-mkdir build2
-cd build2
+    make -j${CPU_COUNT}
+    make install
+    cd ../..
 
-cmake \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLIBCXX_CXX_ABI=libcxxabi \
-  -DLIBCXX_CXX_ABI_INCLUDE_PATHS=$SRC_DIR/libcxxabi/include \
-  -DLIBCXX_CXX_ABI_LIBRARY_PATH=$PREFIX/lib \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -DLLVM_INCLUDE_DOCS=OFF \
-  ..
+    # now rebuild libcxx with libcxxabi
+    mkdir build2
+    cd build2
 
-make -j${CPU_COUNT}
-make install
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXX_CXX_ABI=libcxxabi \
+      -DLIBCXX_CXX_ABI_INCLUDE_PATHS=$SRC_DIR/libcxxabi/include \
+      -DLIBCXX_CXX_ABI_LIBRARY_PATH=$PREFIX/lib \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
+
+    make -j${CPU_COUNT}
+    make install
+
+    cd ..
+else
+    mkdir build
+    cd build
+
+    # on osx we point libc++ to the system libc++abi
+    cmake \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DLIBCXX_CXX_ABI=libcxxabi \
+      -DLIBCXX_CXX_ABI_INCLUDE_PATHS=${CONDA_BUILD_SYSROOT}/usr/include \
+      -DLIBCXX_CXX_ABI_LIBRARY_PATH=${CONDA_BUILD_SYSROOT}/usr/lib \
+      -DLLVM_INCLUDE_TESTS=OFF \
+      -DLLVM_INCLUDE_DOCS=OFF \
+      ..
+
+    make -j${CPU_COUNT}
+    make install
+
+    # on osx we point libc++ to the system libc++abi
+    $INSTALL_NAME_TOOL -change "@rpath/libc++abi.1.dylib" "/usr/lib/libc++abi.dylib" $PREFIX/lib/libc++.1.0.dylib
+
+    cd ..
+fi
