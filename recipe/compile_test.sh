ed1f7c0ffbb6940d4de3e78e793b1328ca92a399
Isuru Fernando <isuruf@gmail.com>
2019-10-19 00:20:54 -0500

    Update compile_test.sh

--- recipe/compile_test.sh
+++ recipe/compile_test.sh
@@ -4,34 +4,34 @@ test -f $PREFIX/include/c++/v1/iterator
 
 LINK_FLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
 
-if [[ `uname -s` != "Darwin" ]]; then
+if [[ "$target_platform" != "osx-64" ]]; then
     LINK_FLAGS="${LINK_FLAGS} -lc++abi"
 fi
 
 FILES=test_sources/*.c
 for f in $FILES
 do
-    $PREFIX/bin/clang -O2 -g $f $LINK_FLAGS
+    clang -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/*.cpp
 for f in $FILES
 do
-    $PREFIX/bin/clang++ -stdlib=libc++ -O2 -g $f $LINK_FLAGS
+    clang++ -stdlib=libc++ -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/cpp11/*.cpp
 for f in $FILES
 do
-    $PREFIX/bin/clang++ -stdlib=libc++ -std=c++11 -O2 -g $f $LINK_FLAGS
+    clang++ -stdlib=libc++ -std=c++11 -O2 -g $f $LINK_FLAGS
     ./a.out
 done
 
 FILES=test_sources/cpp14/*.cpp
 for f in $FILES
 do
-    $PREFIX/bin/clang++ -stdlib=libc++ -std=c++14 -O2 -g $f $LINK_FLAGS
+    clang++ -stdlib=libc++ -std=c++14 -O2 -g $f $LINK_FLAGS
     ./a.out
 done
