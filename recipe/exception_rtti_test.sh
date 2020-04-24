449932e3789a1d34325008dd960b2985cacd2343
Matthew R. Becker <becker.mr@gmail.com>
2020-04-24 13:10:04 -0500

    TST move pybind11 test downstream

--- recipe/exception_rtti_test.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-info=$( ld -v 2>&1 > /dev/null )
-export TEST_LD_VER=`echo "${info}" | perl -wne '/.ld64-(.*?)[^0-9]/ and print "$1\n"'`
-
-pushd test_sources/pybind11_excetion_rtti_test
-make test
-popd
