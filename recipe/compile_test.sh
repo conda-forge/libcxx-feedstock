c171125401b3253a07f503668c18c52ac246a6c4
Isuru Fernando <isuruf@gmail.com>
2020-08-08 02:34:02 +0000

    verbose build and use clang 10

--- recipe/compile_test.sh
+++ recipe/compile_test.sh
@@ -1,15 +1,10 @@
 set -xe
 
-LINK_FLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib"
+LINK_FLAGS="-Wl,-rpath,$PREFIX/lib -L$PREFIX/lib -Wl,-v -v"
 
 # target platform is empty here now
-if [[ `uname -s` != "Darwin" ]]; then
+if [[ "$target_platform" != osx* ]]; then
     LINK_FLAGS="${LINK_FLAGS} -lc++abi"
-else
-  # https://stackoverflow.com/questions/60934005/clang-10-fails-to-link-c-application-with-cmake-on-macos-10-12
-  info=$( ld -v 2>&1 > /dev/null )
-  linkv=`echo "${info}" | perl -wne '/.ld64-(.*?)[^0-9]/ and print "$1\n"'`
-  LINK_FLAGS="${LINK_FLAGS} -mlinker-version=${linkv}"
 fi
 
 FILES=test_sources/*.c
