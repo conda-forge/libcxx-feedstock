a3a0b87fe1a6a259cf3f778b6c0d0444346f3cb2
H. Vetinari <h.vetinari@gmx.com>
2024-07-01 11:24:03 +1100

    point to system libunwind on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -38,4 +38,6 @@ ninja -C build install-cxx install-cxxabi install-unwind
 if [[ "$target_platform" == osx-* ]]; then
     # on osx we point libc++ to the system libc++abi
     $INSTALL_NAME_TOOL -change "@rpath/libc++abi.1.dylib" "/usr/lib/libc++abi.dylib" $PREFIX/lib/libc++.1.0.dylib
+    # same for libunwind
+    $INSTALL_NAME_TOOL -change "@rpath/libunwind.1.dylib" "/usr/lib/system/libunwind.dylib" $PREFIX/lib/libc++.1.0.dylib
 fi
